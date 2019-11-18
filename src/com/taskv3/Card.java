3aa7a7ed953a4aa1aa811db1ae23ca6624abdbd1
ronggenliu <ronggenliu@gmail.com>
2019-11-18 17:20:27 +0800

    updated.

--- src/com/taskv3/Card.java
+++ src/com/taskv3/Card.java
@@ -6,10 +6,21 @@ public class Card implements Comparable {
     private final int suit;
     private final int rank;
 
-    public static final String[] SUITS = {"Clubs", "Diamonds", "Hearts", "Spades"};
-    public static final String[] RANKS = {null, "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+    public final static int ACE = 1;      // Codes for the non-numeric cards.
+    public final static int JACK = 11;    //   Cards 2 through 10 have their
+    public final static int QUEEN = 12;   //   numerical values for their codes.
+    public final static int KING = 13;
+
+
+    public static final String[] SUITS = {"Clubs", "Diamonds", "Hearts", "Spades", "Jokers"};
+    public static final String[] RANKS = {null, "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+    public static final String[] JOKERS = {"black", "red"};
 
     public Card(int rank, int suit) {
+        if(suit < 0 || suit > 3) {
+            throw new IllegalArgumentException("Invalid suit for card");
+        }
+//        if(rank < 0 )
         this.rank = rank;
         this.suit = suit;
     }
